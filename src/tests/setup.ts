b2286a12cc1468eca4ad6e3ece084cfd75467d01
Vadym <vadymlysytskyi@gmail.com>
2025-10-22 15:19:49 +0200

    add enviroment variables in setup

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -2,12 +2,17 @@ import { vi } from "vitest";
 import { pool } from "../db/db.js";
 
 if (process.env.NODE_ENV !== "production") {
-  import("dotenv").then((dotenv) => dotenv.config());
+  import("dotenv").then((dotenv) => dotenv.config({ path: ".env.test" }));
 }
 
 vi.spyOn(console, "log").mockImplementation(() => {});
 vi.spyOn(console, "error").mockImplementation(() => {});
 vi.spyOn(console, "warn").mockImplementation(() => {});
+
+process.env.JWT_SECRET = process.env.JWT_SECRET || "test_jwt_secret";
+process.env.API_KEY = process.env.API_KEY || "test_api_key";
+process.env.TELEGRAM_BOT_TOKEN =
+  process.env.TELEGRAM_BOT_TOKEN || "test_telegram_token";
 
 (globalThis as any).__TEST_DB__ = true;
 
